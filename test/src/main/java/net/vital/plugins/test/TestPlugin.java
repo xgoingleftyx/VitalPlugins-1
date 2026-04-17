6f375923791e52f802731a72df3712186cc0d2df
Chich <52859692+xgoingleftyx@users.noreply.github.com>
2026-04-17 08:34:22 -0400

    Test plugin: switch combat style Accurate->Aggressive

--- test/src/main/java/net/vital/plugins/test/TestPlugin.java
+++ test/src/main/java/net/vital/plugins/test/TestPlugin.java
@@ -6,6 +6,9 @@ import net.runelite.client.eventbus.Subscribe;
 import net.runelite.client.plugins.Plugin;
 import net.runelite.client.plugins.PluginDescriptor;
 
+import vital.api.ui.Combat;
+import vital.api.world.Game;
+
 @Slf4j
 @PluginDescriptor(
 	name = "Test",
@@ -14,9 +17,15 @@ import net.runelite.client.plugins.PluginDescriptor;
 )
 public class TestPlugin extends Plugin
 {
+	private static final int STYLE_ACCURATE   = 1;
+	private static final int STYLE_AGGRESSIVE = 2;
+
+	private boolean switched;
+
 	@Override
 	protected void startUp()
 	{
+		switched = false;
 		log.info("Test plugin started");
 	}
 
@@ -29,5 +38,23 @@ public class TestPlugin extends Plugin
 	@Subscribe
 	public void onGameTick(GameTick event)
 	{
+		if (switched || !Game.isLoggedIn())
+		{
+			return;
+		}
+
+		int currentVarp = Combat.getCombatStyle();
+		log.info("Current combat style varp={} (1-based={})", currentVarp, currentVarp + 1);
+
+		if (currentVarp + 1 != STYLE_ACCURATE)
+		{
+			log.info("Not on Accurate, skipping switch test");
+			switched = true;
+			return;
+		}
+
+		boolean ok = Combat.selectCombatStyle(STYLE_AGGRESSIVE);
+		log.info("selectCombatStyle(Aggressive) returned {}, new varp={}", ok, Combat.getCombatStyle());
+		switched = true;
 	}
 }
